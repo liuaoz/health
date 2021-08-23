01854df2adc8f585bc435989e4ca02626cebd6b1
xianxi.chen <xianxi.chen@signfiy.com>
2021-08-23 14:32:04 +0800

    x

--- src/main/java/com/sun/health/service/baidu/BaiduService.java
+++ src/main/java/com/sun/health/service/baidu/BaiduService.java
@@ -1,19 +1,9 @@
 package com.sun.health.service.baidu;
 
 import com.sun.health.config.BaiduConfig;
-import com.sun.health.core.util.FileUtil;
-import com.sun.health.core.util.SafeUtil;
 import com.sun.health.service.AbstractService;
-import com.tencentcloudapi.common.Credential;
-import com.tencentcloudapi.common.exception.TencentCloudSDKException;
-import com.tencentcloudapi.cvm.v20170312.models.DescribeInstancesResponse;
-import com.tencentcloudapi.ocr.v20181119.OcrClient;
-import com.tencentcloudapi.ocr.v20181119.models.GeneralBasicOCRRequest;
-import com.tencentcloudapi.ocr.v20181119.models.GeneralBasicOCRResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 /**
  * @author Matrix
@@ -25,23 +15,4 @@ public class BaiduService extends AbstractService {
 
     @Autowired
     private BaiduConfig baiduConfig;
-
-
-    public void ocr(String imageUrl) {
-
-        try {
-            Credential cred = new Credential(baiduConfig.getSecretId(), baiduConfig.getSecretKey());
-            OcrClient client = new OcrClient(cred, "ap-shanghai");
-            GeneralBasicOCRRequest req = new GeneralBasicOCRRequest();
-            try {
-                req.setImageBase64(SafeUtil.base64Encode(FileUtil.read(imageUrl)));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            GeneralBasicOCRResponse resp = client.GeneralBasicOCR(req);
-            System.out.println(DescribeInstancesResponse.toJsonString(resp));
-        } catch (TencentCloudSDKException e) {
-            System.out.println(e.toString());
-        }
-    }
 }
