389034ea907bc2177bda38b96068ad056d0c41d5
xianxi.chen <xianxi.chen@signfiy.com>
2021-09-10 15:22:07 +0800

    test

---
+++ src/main/java/com/sun/health/comm/Test.java
@@ -0,0 +1,12 @@
+package com.sun.health.comm;
+
+import java.util.Date;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+    }
+
+}
