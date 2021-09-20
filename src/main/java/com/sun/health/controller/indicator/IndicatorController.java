de2818e444331bb0253f9ec1e75beee06a35c294
matrix <576729125@qq.com>
2021-09-20 09:35:52 +0800

    x

--- src/main/java/com/sun/health/controller/indicator/IndicatorController.java
+++ src/main/java/com/sun/health/controller/indicator/IndicatorController.java
@@ -2,15 +2,14 @@ package com.sun.health.controller.indicator;
 
 import com.sun.health.controller.BaseController;
 import com.sun.health.dto.indicator.IndicatorRespDTO;
+import com.sun.health.entity.blood.BloodReportEntity;
 import com.sun.health.service.indicator.IndicatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/indicator")
@@ -23,5 +22,19 @@ public class IndicatorController extends BaseController {
     public ResponseEntity<List<IndicatorRespDTO>> getTrend(@RequestParam String indicator) {
 
         return ResponseEntity.ok(indicatorService.getTrendByIndicator(indicator));
+    }
+
+    @GetMapping("/reportDate/{reportDate}")
+    public ResponseEntity<List<IndicatorRespDTO>> getByReportNo(@PathVariable String reportDate) {
+
+        List<BloodReportEntity> reportByReportDate = indicatorService.getReportByReportDate(reportDate);
+        List<IndicatorRespDTO> indicatorRespDTOS = reportByReportDate.stream().map(t -> {
+            IndicatorRespDTO dto = new IndicatorRespDTO();
+            dto.setIndicator(t.getItem());
+            dto.setValue(t.getResult());
+            dto.setReportDate(t.getReportDate());
+            return dto;
+        }).collect(Collectors.toList());
+        return ResponseEntity.ok(indicatorRespDTOS);
     }
 }
