f94592c2e048f02d3429ea715366bbabbcf4ed43
matrix <576729125@qq.com>
2022-03-27 08:23:24 +0800

    x

--- src/main/java/com/sun/health/controller/crawler/HousingEstateController.java
+++ src/main/java/com/sun/health/controller/crawler/HousingEstateController.java
@@ -5,6 +5,7 @@ import com.sun.health.controller.BaseController;
 import com.sun.health.core.comm.JsonRet;
 import com.sun.health.core.util.StringUtil;
 import com.sun.health.entity.crawler.shelter.HousingEstateEntity;
+import com.sun.health.service.crawler.CrawlerSource;
 import com.sun.health.service.crawler.house.HousingEstateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,18 +36,23 @@ public class HousingEstateController extends BaseController {
         Date begin = new Date();
         new Thread(() -> {
             while (true) {
+
+                String keyword = StringUtil.randLetter(3);
+//                String first = StringUtil.randHanzi();
+//                String second = StringUtil.randHanzi();
+                if (Objects.isNull(keyword)) {
+                    continue;
+                }
+//                String keyword = first + second;
+                boolean exists = housingEstateService.existsKeyword(keyword, CrawlerSource.AN_JU_KE.getName());
+                if (exists) {
+                    continue;
+                }
                 try {
                     Thread.sleep(new Random().nextInt(3000));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-//                String keyword = StringUtil.randLetter(3);
-                String first = StringUtil.randHanzi();
-//                String second = StringUtil.randHanzi();
-                if (Objects.isNull(first)) {
-                    continue;
-                }
-                String keyword = first;
                 housingEstateService.handleAnJuKe(keyword);
                 Date now = new Date();
                 long delta = now.getTime() - begin.getTime();
@@ -69,11 +75,7 @@ public class HousingEstateController extends BaseController {
         Date begin = new Date();
         new Thread(() -> {
             while (true) {
-                try {
-                    Thread.sleep(new Random().nextInt(3000));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+
 //                String keyword = StringUtil.randLetter(3);
                 String first = StringUtil.randHanzi();
 //                String second = StringUtil.randHanzi();
@@ -81,6 +83,15 @@ public class HousingEstateController extends BaseController {
                     continue;
                 }
                 String keyword = first;
+                boolean exists = housingEstateService.existsKeyword(keyword, CrawlerSource.AN_JU_KE.getName());
+                if (exists) {
+                    continue;
+                }
+                try {
+                    Thread.sleep(new Random().nextInt(3000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 housingEstateService.handleXiaoQuShuo(keyword);
                 Date now = new Date();
                 long delta = now.getTime() - begin.getTime();
